bde74eea7af836438ef6533cb926655b9be68d05
Rytis <lisauskas.rytis@gmail.com>
2021-10-02 21:21:30 +0300

    * Testing.sln: Implemented validators. And added tadas' tests.
    * StringUtility.cs:
    * CharacterLines.cs:
    * EmailValidatorTestsTadas.cs:
    * PhoneValidatorTestsTadas.cs:
    * PasswordValidatorTestsTadas.cs:

---
+++ Testingv2/Source/Utils/CharacterLines.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ValidatorsUnitTests.Source.Utils
+{
+    public static class CharacterLines
+    {
+        public const string SPECIAL_SYMBOL_LINE = "0123456789!@#$%^&*()_+-=:|\\<>,.?/{}[]~";
+        public const string ALL_LETTERS = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+        public const string CAPITAL_LETTERS = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        public const string NUMBERS_ONLY = "0123456789";
+        public const string SPECIAL_CHARS_NO_NUMBERS = "!@#$%^&*()_-=:|\\<>,.?/{}[]~";
+    }
+}
---
+++ Testingv2/Source/Utils/StringUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ValidatorsUnitTests.Source.Utils
+{
+    public static class StringUtility
+    {
+        public static bool FindCharactersInLine(string password, string line)
+        {
+ 
+            for (int currentLenght = 0; currentLenght < password.Length; currentLenght++)
+            {
+                if (line.Contains(password[currentLenght]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
