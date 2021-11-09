8396d831fb285ccd3409e7d2d4d42d17fe1d75e9
Rytis <lisauskas.rytis@gmail.com>
2021-11-09 11:04:04 +0200

    all the files left

---
+++ Testingv2/Source/Base/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ValidatorsUnitTests.Source.Base
+{
+    [Serializable]
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+
+        public User(string name, string surname, string email, string number, string password, string address) {
+            this.Name = name;
+            this.Surname = surname;
+            this.Email = email;
+            this.PhoneNumber = number;
+            this.Password = password;
+            this.Address = address;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Surname + " " + Email + " " + PhoneNumber + " " + Password + " " + Address; 
+        }
+    }
+}
---
+++ Testingv2/Source/Base/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ValidatorsUnitTests.Source.Base
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            UI ui = new UI();
+            ui.FetchData();
+            ui.StartMenu();
+            ui.SendData();
+        }
+    }
+}
